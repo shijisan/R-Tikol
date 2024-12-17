5cabc7dd789c428b949e673e7da0c5c231797f32
shijisan <cjsantos2411638@gmail>
2024-12-18 05:42:47 +0800

    fix manage authors route

--- app/api/admin/route.tsx
+++ app/api/admin/route.tsx
@@ -1,17 +1,25 @@
 import prisma from "@/lib/prisma"
 
 export async function GET(){
-   const admin = await prisma.user.findMany({
-      where:{role :"ADMIN"},
-      select:{
-         id: true,
-         fullName: true,
-         email: true,
-         role: true,
-         createdAt: true,
-         updatedAt: true,
-      }
-   });
-
-   return admin;
+   try{
+      const admins = await prisma.user.findMany({
+         where:{role :"ADMIN"},
+         select:{
+            id: true,
+            fullName: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            updatedAt: true,
+         }
+      });
+   
+      return new Response(JSON.stringify(admins), {
+         status: 200,
+         headers: {"Content-type" : "application/json"},
+      });
+   }
+   catch(error){
+      console.error(error);
+   }
 }
